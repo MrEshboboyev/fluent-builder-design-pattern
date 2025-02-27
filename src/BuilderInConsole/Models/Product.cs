5faa934555807f3247615f6cccb1cd2d22327ba4
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-27 16:14:28 +0500

    Builder Pattern in Console application : with director, product and more

---
+++ src/BuilderInConsole/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace BuilderPatternInConsole.Models;
+
+public class Product
+{
+    private List<object> _parts = [];
+
+    public void Add(string part)
+    {
+        _parts.Add(part);
+    }
+
+    public string ListParts()
+    {
+        string str = string.Empty;
+
+        for (int i = 0; i < _parts.Count; i++)
+        {
+            str += _parts[i] + ", ";
+        }
+
+        str = str[..^2]; // removing last ",c"
+
+        return "Product Parts: " +str + "\n";
+    }
+}
